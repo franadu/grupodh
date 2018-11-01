680acc3d0b204de259e290b2856da10884f86a42
franadu <franciscoaduriz@gmail.com>
2018-11-01 19:54:52 -0300

    Envio nuevo push con Mysql falta agregar curretntimestamp en created at

--- Classes/Mysql.php
+++ Classes/Mysql.php
@@ -1,11 +1,68 @@
 <?php
-	require 'Db.php';
-
+	require 'Json.php';
 	class Mysql extends Db
 	{
 		/*$dsn = "mysql:host=127.0.0.1;dbname=grupodh;charset=UTF8;port=3306";
 		$user="root";
 		$pass="";*/
+		protected function guardarUsuario($usuario, $imagen="")
+		{
+			$name=[];
+			$last_name=[];
+			$mail=[];
+			$username=[];
+			$phone=[];
+			$password=[];
+			$image=[];
+			$cont=0;
+			for ($i=0; $i <count($usuario["usuario"]) ; $i++) {
+				$name[]=$usuario["usuario"][$i]["nombre"];
+				$last_name[]=$usuario["usuario"][$i]["apellido"];
+				$mail[]=$usuario["usuario"][$i]["mail"];
+				$username[]=$usuario["usuario"][$i]["username"];
+				if (!empty($usuario["usuario"][$i]["tel"])){$phone[]=$usuario["usuario"][$i]["tel"];} else {$phone[]="null";}
+				$password[]=$usuario["usuario"][$i]["contra"];
+				$image[]=$usuario["usuario"][$i]["avatar"];
+				$cont++;
+			}
+
+			$instancia=[];
+			for ($i=0; $i <$cont ; $i++) {
+				if ($i===0){
+					$instancia[$i]="('".$name[$i]."','".$last_name[$i]."','".$mail[$i]."','".$username[$i]."',".$phone[$i].",'".$password[$i]."','".$image[$i]."')";
+				} else {
+					$instancia[$i]=",('".$name[$i]."','".$last_name[$i]."','".$mail[$i]."','".$username[$i]."',".$phone[$i].",'".$password[$i]."','".$image[$i]."')";
+				}
+			}
+
+			$unstringparahacerununicoinsert="";
+			foreach ($instancia as $key => $value) {
+				$unstringparahacerununicoinsert.=$value;
+			}
+
+			$dsn="mysql:host=127.0.0.1;dbname=grupodh;charset=UTF8;port=3306";
+			$user="root";
+	    $pass="";
+	    $opt=[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+	    $db=new PDO ($dsn,$user, $pass, $opt);
+			try
+			{
+	      $query = $db->query("insert into user  (name,last_name,mail,username,phone,password,image) values $unstringparahacerununicoinsert");
+				$results=$query->fetchAll(PDO::FETCH_ASSOC);
+			}
+			catch (PDOException $a)
+			{
+				echo $a->getMessage();
+			}
+		}
+
+		static public function migracionUsuariosDeJsonAMysql()
+		{
+			$archivo=Json::connector();
+			$archivo=json_decode($archivo, true);
+			self::guardarUsuario($archivo);
+		}
+
 		static public function createTables($dsn,$user,$pass)
 		{
 			$opt=[ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
@@ -17,7 +74,8 @@
 				create table user(id int unsigned primary key auto_increment unique, created_at timestamp,	updated_at timestamp NULL DEFAULT NULL, name varchar(30) not null, last_name varchar (30) not null, username varchar(30) not null unique, image varchar (100) default null, mail varchar(50) not null, phone varchar(35) default null, password varchar (100) not null, isset tinyint unsigned default 1) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 				create table cart(id int unsigned primary key auto_increment unique,created_at timestamp, total float unsigned not null) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci; create table user_cart( id int unsigned primary key auto_increment unique, created_at timestamp,    updated_at timestamp null default null, id_username int unsigned not null, id_cart int unsigned not null, FOREIGN KEY (id_username) references user(id), FOREIGN KEY (id_cart) references cart(id)) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 				create table cart_product(id int unsigned primary key auto_increment unique, 	created_at timestamp, updated_at timestamp null default null, id_product int unsigned not null, id_cart int unsigned not null, FOREIGN KEY (id_product) references product(id), FOREIGN KEY (id_cart) references cart(id)) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-				create table product_categotry (id int unsigned primary key auto_increment unique, created_at timestamp, updated_at timestamp null default null,	id_product int unsigned not null, id_category int unsigned not null, FOREIGN KEY (id_product) references product(id), FOREIGN KEY (id_category) references category(id)) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;")
+				create table product_categotry (id int unsigned primary key auto_increment unique, created_at timestamp, updated_at timestamp null default null,	id_product int unsigned not null, id_category int unsigned not null, FOREIGN KEY (id_product) references product(id), FOREIGN KEY (id_category) references category(id)) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;");
+				$results = $query->fetchAll(PDO::FETCH_ASSOC);
 			}
 			catch (PDOException $a)
 			{
@@ -36,18 +94,6 @@
 			}
 			return $db;
 		}
-
-		public function mysqlGuardarObjetos($dsn,$user,$pass,$object)
-		{
-			/*Consigo el nombre del objeto que va a ser instanciado(guardado en la tabla de su propio nombre)*/
-			$nombre = get_class($object);
-			/*Recorro el objeto para ver si tiene un array o más dentro suyo arranco un contador para ver la cantidad de arrays que hay dentro de ese objeto */
-			/*asi seria ad eternum pero solo voy a tener un array dentro de un objeto por el momento*/
-			$db = self::mysqlDB($dsn,$user,$pass);
-			$query=$db->query("insert into $nombre ( ,  )")
-
-		}
 	}
-
 
  ?>
