b7c2eaa38494e75a9c37894b6b39caa5ef72c808
AndresMndez <andresmndeze@gmail.com>
2018-11-07 15:35:17 -0300

    eee

--- funciones/variablesmysql.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*lafuncion de esto es que estas variables se pasen a Mysql al copíar los datos y que al testear como programador pueda ingresarlas en donde uno quiera*/
-$dsn="mysql:host=127.0.0.1;dbname=grupodh;charset=UTF8;port=3306";
-$user="root";
-$pass="";
-?>
